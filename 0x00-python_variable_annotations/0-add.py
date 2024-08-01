d7a8a5d7326d1fc777de8446479b12ac417137eb
AerinLom <aerinlom1@gmail.com>
2024-08-01 06:33:06 -0700

    Task 0: Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add that takes floats a and b
+"""
+
+def add(a: float, b: float) -> float:
+   """
+   a type-annotated function that returns the sum of a and b
+   """
+   return a + b
