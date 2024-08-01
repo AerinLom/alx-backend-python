fbca4734e8547ca5a1654db5439cb5564b24ca86
AerinLom <aerinlom1@gmail.com>
2024-08-01 07:29:06 -0700

    Task 8: Multiplier

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@ Type-annotated function to_kv that takes a string and a number as arguments.
 """
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple with the string and the square of the number as a float.
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function make_multiplier that takes a float and returns a multiplier function.
+"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by the given multiplier.
+    """
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    
+    return multiplier_function
