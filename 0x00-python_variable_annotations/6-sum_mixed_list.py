c70c81b9092351179b007b4e2f90ca705bc43cc0
AerinLom <aerinlom1@gmail.com>
2024-08-01 07:22:14 -0700

    Task 7: to kv

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ type-annotated function floor that takes a mixed list as an arg
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     a type-annotated function that returns the sum of a mixed list
     """
