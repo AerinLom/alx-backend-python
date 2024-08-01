ac8a56a1e96be32e6d6f7e1409642b23dca38309
AerinLom <aerinlom1@gmail.com>
2024-08-01 07:25:26 -0700

    Task 7: To KV

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,4 +8,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple with the string and the square of the number as a float.
     """
-    return (k, float(v * v))
+    return (k, (v * v))
