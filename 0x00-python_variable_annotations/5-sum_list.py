fb4be454c252036a9005597462854786101e2635
AerinLom <aerinlom1@gmail.com>
2024-08-01 07:09:13 -0700

    Task 6: Mixed list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-type-annotated function floor that takes float n as an arg
+type-annotated function floor that takes a list as an arg
 """
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-    a type-annotated function that returns the floor of n
+    a type-annotated function that returns the sum of a list 
     """
     return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor that takes a mixed list as an arg
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    a type-annotated function that returns the sum of a mixed list
+    """
+    return sum(mxd_lst)
