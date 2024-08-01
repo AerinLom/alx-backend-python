006694548885ff157437d516f2fc7504a63bb51b
AerinLom <aerinlom1@gmail.com>
2024-08-01 07:04:09 -0700

    Task list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor that takes float n as an arg
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    a type-annotated function that returns the floor of n
+    """
+    return sum(input_list)
