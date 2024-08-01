4f859fe44ae17214b32786139e95e12b7efd709a
AerinLom <aerinlom1@gmail.com>
2024-08-01 06:48:07 -0700

    Task 3: To string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes float n as an arg
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    a type-annotated function that returns the string of n
+    """
+    return str(n)
