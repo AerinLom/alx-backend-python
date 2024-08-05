efd398c58b9971a118780e128771231f84692174
AerinLom <aerinlom1@gmail.com>
2024-08-05 05:52:37 -0700

    Task 4: Task delays

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module defining an asynchronous coroutine, using delays concurrently using tasks.
+Module defining asynchronous coroutine, using delays concurrently using tasks
 """
 
 import asyncio
@@ -10,7 +10,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous coroutine that spawns task_wait_random n times with a set delay.
+    Asynchronous coroutine that spawns task_wait_random n times with set delays
     """
     current_tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(current_tasks)]
+    task_delays = await asyncio.gather(*current_tasks)
+    return sorted(task_delays)
